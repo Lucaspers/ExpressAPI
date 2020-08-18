f613caaab46d945bc2d10d46bbefdf9335b6dfc3
Lucaspers <dunderdykaren@hotmail.com>
2020-08-18 10:39:58 +0200

    now able to do request to api and print from object

---
+++ config.js
@@ -0,0 +1,8 @@
+const constants = {
+    openWeatherMap: {
+        BASE_URL: 'https://api.openweathermap.org/data/2.5/weather?q=',
+        SECRET_KEY: 'fa162aeaf485ce01f980a0cdc40dfc3f'
+    }
+}
+
+module.exports= constants;
--- src/app.js
+++ src/app.js
@@ -1,13 +1,44 @@
 const express = require('express');
+const hbs = require('hbs');
+const path = require('path');
+const weatherData = require('../utils/weatherData');
 const app = express();
+
 const port = process.env.PORT || 3000
+
+const publicStaticDirPath = path.join(__dirname, '../public')
+const viewsPath = path.join(__dirname, '../templates/views');
+const partilsPath = path.join(__dirname, '../templates/partials');
+
+
+app.set('view engine', 'hbs');
+app.set('views', viewsPath);
+hbs.registerPartials(partilsPath);
+app.use(express.static(publicStaticDirPath));
+
 
 app.get('', (req, res) =>{
     res.send('hi this is expressapi');
 })
 
+//localhost:3000/weather?address=london
 app.get('/weather', (req, res) => {
-    res.send('this is weather endpoint');
+
+    const address = req.query.address
+
+    weatherData(address, (error, {temperature, description, cityName}) => {
+        if(error) {
+            return res.send({
+                error
+            })
+        }
+        console.log(temperature, description, cityName);
+        res.send({
+            temperature,
+            description,
+            cityName
+        })
+    })
 })
 
 app.get('*', (req, res) => {
